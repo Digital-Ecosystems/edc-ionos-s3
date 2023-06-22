f4c044c6b1bf32b9d0c2307f860b8b30a3ef2373
Desislav Arashev <desislav.arashev@daiteap.com>
2023-06-22 09:20:20 +0300

    Add remove tfstate in terraform-destroy script

--- deployment/terraform/destroy-services.sh
+++ deployment/terraform/destroy-services.sh
@@ -23,6 +23,28 @@ cd ../ionos-s3-deploy
 terraform init
 terraform destroy -auto-approve
 
+cd ../
+
+# remove terraform state
+rm -rf ./configure-ionos-s3-webhook-address/.terraform
+rm -f ./configure-ionos-s3-webhook-address/terraform.tfstate
+rm -f ./configure-ionos-s3-webhook-address/.terraform.lock.hcl
+rm -f ./configure-ionos-s3-webhook-address/terraform.tfstate.backup
+
+rm -rf ./ionos-s3-deploy/.terraform
+rm -f ./ionos-s3-deploy/terraform.tfstate
+rm -f ./ionos-s3-deploy/.terraform.lock.hcl
+rm -f ./ionos-s3-deploy/terraform.tfstate.backup
+
+rm -rf ./vault-init/.terraform
+rm -f ./vault-init/terraform.tfstate
+rm -f ./vault-init/.terraform.lock.hcl
+rm -f ./vault-init/terraform.tfstate.backup
+
+rm -rf ./vault-deploy/.terraform
+rm -f ./vault-deploy/terraform.tfstate
+rm -f ./vault-deploy/.terraform.lock.hcl
+rm -f ./vault-deploy/terraform.tfstate.backup
+
 kubectl --kubeconfig $TF_VAR_kubeconfig delete namespace $TF_VAR_namespace
 rm vault-init/vault-keys.json
-
