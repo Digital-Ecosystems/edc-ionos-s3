e556743c0f7cfe90e49c70ddbe4a5d644b3175f9
Glaucio Jannotti <glaucio.jannotti@dengun.com>
2023-12-12 14:37:42 -0300

    fix: shared bucket access denied (#56)

--- edc-ionos-extension/core-ionos-s3/src/main/java/com/ionos/edc/extension/s3/api/S3ConnectorApi.java
+++ edc-ionos-extension/core-ionos-s3/src/main/java/com/ionos/edc/extension/s3/api/S3ConnectorApi.java
@@ -14,36 +14,22 @@
 
 package com.ionos.edc.extension.s3.api;
 
-import io.minio.Result;
-import io.minio.messages.Item;
 import org.eclipse.edc.runtime.metamodel.annotation.ExtensionPoint;
 
 import com.ionos.edc.extension.s3.connector.ionosapi.TemporaryKey;
 
 import java.io.ByteArrayInputStream;
 
-
-
 @ExtensionPoint
 public interface S3ConnectorApi {
 
-    void s3ConnectorApi(String endpoint, String accessKey, String secretKey, String token);
-
     void createBucket(String bucketName);
 
-    void deleteBucket(String bucketName);
-
     boolean bucketExists(String bucketName);
-
-    void uploadFile(String bucketName, String fileName, String path);
     
     void uploadParts(String bucketName, String fileName, ByteArrayInputStream  part);
     
     byte[] getFile(String bucketName, String fileName);
-
-    Result<Item> listItems(String bucketName);
-
-    void deleteFile(String bucketName, String fileName);
     
     TemporaryKey createTemporaryKey();
     
