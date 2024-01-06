d6afc4b440292a5a19bf76b19c0b51a979a7bfd1
Glaucio Jannotti <glaucio.jannotti@dengun.com>
2024-01-06 18:48:21 -0300

    chore: removing examples folders

--- example/file-transfer-push-daps/terraform/build-project/main.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "build-project" {
-  provisioner "local-exec" {
-    command = "cd ../../../../ && ./gradlew clean build"
-    interpreter = ["bash", "-c"]
-  }
-}
--- example/file-transfer-push-daps/terraform/vault-init/main.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "vault-init" {
-  provisioner "local-exec" {
-    command = "${path.module}/vault-init.sh"
-    interpreter = ["bash", "-c"]
-  }
-}
